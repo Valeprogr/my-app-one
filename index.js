4603e9633e33f10cf3bb70886a4dc3abd9556987
valeviit <valeviit@icloud.com>
2022-11-17 14:42:02 +0100

    add a app.route and diff. request

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
+import express from 'express';
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.route('/')
+.get((req, res) => {res.send('This is a GET REQUEST!')})
+.post((req, res) => {res.send('This is a POST REQUEST!')})
+.put((req, res) => {res.send('This is a PUT REQUEST!')})
+.delete((req, res) => {res.send('This is a DELETE REQUEST!')})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
